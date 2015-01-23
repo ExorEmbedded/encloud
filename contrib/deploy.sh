48a65608f84cdb807b2ec3630afe0508cdc3d38e
Steven Dorigotti <s.dorigotti@endian.com>
2015-01-23 16:49:26 +0100

    contrib: deploy script (ECE,SECE)

---
+++ contrib/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+#
+# Deploy ECE/SECE files based on local installation.
+# (DEVELOPMENT ONLY)
+
+[ -z ${MODE} ] && MODE="ece"
+[ -z ${HOST} ] && HOST="192.168.122.132"
+
+# copy global libencloud settings (for license)
+[ "${MODE}" = "sece" ] && scp /etc/xdg/Exor/libencloud.conf usom:/etc/qt4/Exor/libencloud.conf
+
+# copy initialization certificates
+ssh usom mkdir -p /var/lib/encloud
+scp /var/lib/encloud/init* usom:/var/lib/encloud
+
+# copy switchboard host and restart service
+ssh usom "/etc/init.d/encloud restart; \
+        grep switchboard-host /etc/hosts 2>/dev/null || \
+            echo "${HOST} switchboard-host" >> /etc/hosts"
