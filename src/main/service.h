3ce31897ecc96343039d684ebbc471d2b2bbab17
Steven Dorigotti <s.dorigotti@endian.com>
2017-05-31 13:11:33 +0200

    [TASK] CONNECT-416 Update and merge Exor brand onto master
    renamed "Exor Encloud Service" to "Exor Connect Service"

--- src/main/service.h
+++ src/main/service.h
@@ -8,13 +8,7 @@
 #include "application.h"
 #include <encloud/Logger>
 
-#ifdef ENCLOUD_EXOR
-#  define ENCLOUD_SVC_NAME  (ENCLOUD_APP_FULL + QString(" Service"))
-#else
-#  define ENCLOUD_NEW_NAME    "Connect Service"
-// changed to follow new Connect App naming
-#  define ENCLOUD_SVC_NAME  ENCLOUD_ORG " " ENCLOUD_NEW_NAME
-#endif
+#define ENCLOUD_SVC_NAME  ENCLOUD_ORG " " "Connect Service"
 
 #define ENCLOUD_SVC_DESC    (ENCLOUD_SVC_NAME + QString(" provides an API for Cloud functionality"))
 
